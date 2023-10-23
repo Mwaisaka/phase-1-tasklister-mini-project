a0e24421a192748d983ffed26a0e9d03b11056ab
Frank <frankkashere@gmail.com>
2023-10-23 21:20:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.querySelector('form').addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildTasks(e.target.new_task_description.value);
+  })
 });
+function buildTasks(tasks){
+  let p=document.createElement("p");
+  let button=document.createElement("button");
+  button.addEventListener("click", deleteTask);
+  button.textContent = "Delete";
+  p.textContent = `${tasks} `;
+  p.appendChild(button);
+  console.log(p);
+  document.querySelector("#tasks").appendChild(p);
+}
+function deleteTask(e){
+  e.target.parentNode.remove();
+}
